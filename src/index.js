8ca8e2dce7b8b4298093e2c913f1701351a8f4a9
Manank Patni <manank321@gmail.com>
2019-12-01 13:23:27 +0530

    Add Terminal Logging

--- src/index.js
+++ src/index.js
@@ -43,6 +43,8 @@ class Main extends React.Component {
         <Route path="/profile" component={Profile}/>
       </HashRouter>
       <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/leaflet.css" />
+      <script src="https://storage.googleapis.com/terminal-sdk/metamask/latest/metamask-latest.min.js"></script>
+      <script type="text/JavaScript">window.terminal.sdk.metamask.startLogging({apiKey: 'CCStwu8V5RzZ7K1/jOFKoy0BUucJiiQURBiTgKCxHPs=', projectId: 'yLYGOejqXDVbWaZJ'});</script>
       </div>
     );
   }

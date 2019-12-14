490eb88efb53487bf51ab86a5935593e953c97eb
Abhimanyu <abhimanyu@pop-os.localdomain>
2019-12-14 16:00:20 +0530

    trying to fix check in

--- src/ticketlist.js
+++ src/ticketlist.js
@@ -1,16 +1,121 @@
 import React from "react";
-import { Container,Row,Col, Card,CardHeader,CardTitle,CardBody,CardImg } from "shards-react";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
+import Loader from 'react-loader-spinner'
+import WalletConnect from "@walletconnect/browser";
+import WalletConnectQRCodeModal from "@walletconnect/qrcode-modal";
+import TickesOnChain from "./contracts/TicketsOnChain.json";
+import Web3Connect from "web3connect";
+import Web3 from "web3";
+import WalletConnectProvider from "@walletconnect/web3-provider";
+import { Container,Row,Col, Card,CardHeader,CardTitle,CardBody,CardImg, Button } from "shards-react";
 export default class CreateToken extends React.Component{
   constructor(props){
     super(props);
     this.state ={
       events:[],
       flag:false,
+      zero:false,
+      web3: null,
+      instance:null,
       accounts: this.props.account,
       superWeb3: this.props.web3,
-      superContract:this.props.contract
+      superContract:this.props.contract,
+      walletConnector: new WalletConnect({
+        bridge: "https://bridge.walletconnect.org" // Required
+      }),
     }
+    //this.state.walletConnector.killSession();
   }
+  checkIn = async(id)=>{
+    const sleep = (milliseconds) => {
+      return new Promise(resolve => setTimeout(resolve, milliseconds))
+    }
+   // let provider = await Web3Connect.ConnectToInjected();
+    //  provider = await Web3Connect.ConnectToWalletConnect(
+    //   WalletConnectProvider,
+    //   {
+    //     infuraId: "311ef590f7e5472a90edfa1316248cff", // required
+    //     bridge: "https://bridge.walletconnect.org" // optional
+    //   }
+    // );
+    //console.log(provider);
+    // await provider.close();
+    //console.log(provider);
+     let provider =  new WalletConnectProvider({
+      infuraId: "311ef590f7e5472a90edfa1316248cff"
+    });
+   
+    await provider.enable()
+    const web3 = new Web3(provider);
+      let acc = await web3.eth.getAccounts();
+      const deployedNetwork = TickesOnChain.networks[42];
+      const instance = new web3.eth.Contract(
+        TickesOnChain.abi,
+        deployedNetwork && deployedNetwork.address,
+      );
+      await sleep(500); 
+      console.log(acc[0]);
+      await sleep(500); 
+      let response = await instance.methods.checkIn(id).send({from: acc[0] });
+      this.setState({web3:web3,instance:instance});
+      this.callfunc(id);
+  }
+  callfunc = async (id)=>{
+    const {web3,instance} = this.state;
+    const acc = await web3.eth.getAccounts();
+    await instance.methods.checkIn(id).send({from: acc[0] });
+  }
+  
+  // checkIn = async (id) => {
+  //   const sleep = (milliseconds) => {
+  //     return new Promise(resolve => setTimeout(resolve, milliseconds))
+  //   }
+  //   const {superContract,superWeb3,walletConnector} = this.state;
+ 
+  //   console.log("wallet");
+  //   console.log(walletConnector);
+  //   walletConnector.killSession();
+  //   await sleep(500);
+  //   walletConnector.createSession().then(async () => {
+  //       // get uri for QR Code modal
+  //       const uri = walletConnector.uri;
+  //       // display QR Code modal
+  //       WalletConnectQRCodeModal.open(uri, async () => {
+  //         console.log("QR Code Modal closed");
+  //       });
+  //   });
+    
+  //    walletConnector.on("connect", async (error, payload) => {
+  //     if (error) {
+  //       //throw error;
+  //     }
+    
+  //     // Close QR Code Modal
+  //     WalletConnectQRCodeModal.close();
+    
+  //     // Get provided accounts and chainId
+  //     const { accounts, chainId } = payload.params[0];
+  //    let encoded = superContract.methods.checkIn(id).encodeABI();
+  //    console.log(encoded);
+  //     let tx = {
+  //       from: accounts[0], // Required
+  //       to: TickesOnChain.networks[42].address, // Required (for non contract deployments)
+  //       data: encoded
+  //     }
+  //      walletConnector
+  //     .sendTransaction(tx)
+  //     .then(result => {
+  //       // Returns transaction id (hash)
+  //       console.log(result);
+  //     })
+  //     .catch(error => {
+  //       // Error returned when rejected
+  //       console.error(error);
+  //     });
+  //   });
+    
+  // }    
+  
   fetchHostedNetwork = async ()=>{
    
     const {superWeb3,superContract} = this.state;
@@ -20,6 +125,9 @@ export default class CreateToken extends React.Component{
     const accounts = await superWeb3.eth.getAccounts();
     let response = await superContract.methods.UserProfile().call({from:accounts[0]});
     console.log(response[0].length);
+    if(response[0].length==0){
+      this.state({zero:true});
+    }
     for(let i =0; i<response[0].length;i++){
       let event = await superContract.methods.eventMapping(response[0][i]).call();
       _events.push(event);
@@ -31,8 +139,60 @@ export default class CreateToken extends React.Component{
   
     
     if(this.state.superWeb3!= null && this.state.superContract !=null){
+      
       if(!this.state.flag)
-        {this.fetchHostedNetwork();}
+        {
+          this.fetchHostedNetwork();
+          
+        }
+        let listHost;
+        if(!this.state.flag){
+          listHost = <center> <h6> Loading..: <Loader
+          type="Puff"
+          color="#00BFFF"
+          height={100}
+          width={100}
+          timeout={3000} //3 secs
+  
+       /></h6></center>;
+        }
+        else if(this.state.zero){
+          listHost =  <Container className="main-container">
+          <Row>
+            <Col sm="12" md="12">
+              <div>
+        
+                <Card>
+                 
+                  
+                  <CardBody className="WEB3">
+                 <center>   <CardTitle>You are not hosting any event</CardTitle>
+                    </center>
+                  </CardBody>
+                </Card>
+              </div>
+            </Col>
+          </Row>
+        </Container>;
+        }
+        else {
+          listHost = this.state.events.map((item, index)=>
+              <Card style={{marginTop: "30px"}}>
+              <CardHeader>Event</CardHeader>
+              <CardBody className="Ticket">
+                <CardTitle>{item[4]}</CardTitle>
+                <CardTitle>{item[9]}</CardTitle>
+                <Button>Withdraw Amount</Button>
+                <t/><t/>
+                <Button onClick={async ()=>{await this.checkIn(item[10])}}>Start Checking in</Button>
+                <t/><t/>
+                <Button>End Event</Button>
+              </CardBody>
+            </Card>
+        );
+        }
+       
+        
       return(
         <div>
           <Container className="main-container">
@@ -40,14 +200,7 @@ export default class CreateToken extends React.Component{
               <Col sm="12" md="12">
                 <div>
                   <h3>Events you are hosting</h3><hr/> <br />
-                  <Card>
-                    <CardHeader>Ticket</CardHeader>
-                    <CardBody className="Ticket">
-                      <CardTitle>Place:</CardTitle>
-                      <CardTitle>Date:</CardTitle>
-                      <CardTitle>Purchased in:</CardTitle>
-                    </CardBody>
-                  </Card>
+                  {listHost}
                 </div>
               </Col>
             </Row>

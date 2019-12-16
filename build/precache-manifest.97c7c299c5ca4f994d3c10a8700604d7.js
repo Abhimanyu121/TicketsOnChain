c61f0e52c533c198367ac933397965cce700221c
Abhimanyu <abhimanyu@pop-os.localdomain>
2019-12-17 00:57:32 +0530

    added some logos

---
+++ build/precache-manifest.97c7c299c5ca4f994d3c10a8700604d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91248cef0a05c49d1056c48699b2efc9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "dd05a1059dccf371f362",
+    "url": "./static/css/2.94c77688.chunk.css"
+  },
+  {
+    "revision": "5365b06917f3d0bee00d",
+    "url": "./static/css/main.6a393a82.chunk.css"
+  },
+  {
+    "revision": "dd05a1059dccf371f362",
+    "url": "./static/js/2.901da840.chunk.js"
+  },
+  {
+    "revision": "5365b06917f3d0bee00d",
+    "url": "./static/js/main.61abb740.chunk.js"
+  },
+  {
+    "revision": "7a99a66aab7807fee24b",
+    "url": "./static/js/runtime-main.e26afdc4.js"
+  }
+]);

79d1cf469cdf7c5e374902be2b0d1445119f3710
Abhimanyu <abhimanyu@pop-os.localdomain>
2019-12-16 22:11:39 +0530

    added build files for deployment

---
+++ build/precache-manifest.244b49bb1f2b95606b9464ec58eb4852.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a60028198dc496da680cfd12ffa4f684",
+    "url": "./index.html"
+  },
+  {
+    "revision": "dd05a1059dccf371f362",
+    "url": "./static/css/2.94c77688.chunk.css"
+  },
+  {
+    "revision": "a8c40d1942e674f25055",
+    "url": "./static/css/main.6a393a82.chunk.css"
+  },
+  {
+    "revision": "dd05a1059dccf371f362",
+    "url": "./static/js/2.901da840.chunk.js"
+  },
+  {
+    "revision": "a8c40d1942e674f25055",
+    "url": "./static/js/main.c62a7b12.chunk.js"
+  },
+  {
+    "revision": "7a99a66aab7807fee24b",
+    "url": "./static/js/runtime-main.e26afdc4.js"
+  }
+]);

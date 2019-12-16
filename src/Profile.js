92f27be897aa1749fd60bc60c45a2327f0baa5b9
Abhimanyu <abhimanyu@pop-os.localdomain>
2019-12-16 14:42:36 +0530

    fixing profile

--- src/Profile.js
+++ src/Profile.js
@@ -1,5 +1,6 @@
 import React from "react";
-import Box from "3box";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
+import Loader from 'react-loader-spinner'
 import {
   Row,
   Col,
@@ -11,6 +12,8 @@ import {
   CardImg,
   ListGroup,
 } from 'shards-react';
+
+const Box = require('3box')
 export default class Profile extends React.Component {
   constructor(props){
     super(props);
@@ -19,41 +22,125 @@ export default class Profile extends React.Component {
       superContract:this.props.contract,
       space:null,
       account:null,
-      box:null
-
+      box:null,
+      contractProfile:null,
+      boxProfile:{},
+      loadState:0
     }
     
+    
+  }
+  isEmpty=(obj) =>{
+    for(var prop in obj) {
+        if(obj.hasOwnProperty(prop))
+            return false;
+    }
+
+    return true;
+}
+
+  fetchContract=async()=>{
+    const{superWeb3,superContract} = this.state;
+    const accounts = await superWeb3.eth.getAccounts();
+    let response = await superContract.methods.UserProfile().call({from:accounts[0]});
+    this.setState({contractProfile:response,loadState:this.state.loadState+1})
   }
   fetchProfile=async ()=>{
-      const acc = await this.state.superWeb3.eth.getAccounts();
-    console.log(this.state.superWeb3.currentProvider);
-    const box = await Box.openBox(acc[0], this.state.superWeb3.currentProvider);
-    await box.public.set("name","fuckingname");
-    let ac= await box.public.get("name");
-    console.log(ac);
-    const space = await box.getSpace(acc[0], 'spacesdemo')  
-    this.setState({box:box,space:space,account:acc[0]});
+    const acc = await this.state.superWeb3.eth.getAccounts();
+    console.log(this.state.superWeb3)
+    let  spaceList = await Box.getSpace(acc[0],"ToC");
+    //const box = await Box.openBox(acc[0], this.state.superWeb3.currentProvider)
+    
+    
+    console.log(spaceList);
+    if(this.isEmpty(spaceList)){
+      console.log("flying")
+      spaceList={
+        name:"You need to set Profile",
+        phone :"You need to set profile",
+        email : "You need to se Profile"
+      }
+    }
+    this.setState({boxProfile:spaceList,loadState:this.state.loadState+1});
   }
   render() {
  
    
   
  if(this.state.superWeb3!= null && this.state.superContract !=null){
-  
-      return (
-        <div style={{fontFamily: "Cambria, Cochin, Georgia, Times, 'Times New Roman', serif"}}>
-          <Container className="main-container">
+   if(this.state.loadState >=2){
+    return (
+        
+      <div style={{fontFamily: "Cambria, Cochin, Georgia, Times, 'Times New Roman', serif"}}>
+        <Container className="main-container">
+        <Row>
+         
+        <Col sm="12" md="12">
+        <div>
+      <Row>
+        <Col sm="12" md="3">
+            <CardImg width="200" src="https://c.gitcoin.co/avatars/0357f94b529985a8a898ab338add0edf/djrosenbaum.png" /> <br /> <br/>
+    <h3><b>{this.state.boxProfile.name}</b></h3>
+            <h5><b><a href="/#/edit-profile" style={{color: "white"}}>Edit Profile</a></b></h5>
+        </Col>
+        <Col sm="12" md="9">
+          <div>
+            <Card>
+              <CardHeader>Details</CardHeader>
+              <CardBody>
+              <p>Phone Number :- {this.state.boxProfile.phone}</p>
+              <p>Email :- {this.state.boxProfile.email}</p>
+    <p>Total Tickets :- {this.state.contractProfile[1].length}</p>
+                <p>Events Hosted :-  {this.state.contractProfile[0].length}</p>
+      
+                </CardBody>
+              </Card>
+          </div>
+        </Col>
+      </Row>
+      <br />
+      <br />
+    <hr />
+      <h5>Events You have Added</h5>
+      <Row>
+        <ListGroup>
+          
+        </ListGroup>
+      </Row>
+
+      <br />
+        <hr />
+          <h5>Your Tickets</h5>
           <Row>
+
            
-          <Col sm="12" md="12">
-      <ProfileView></ProfileView>
-          </Col>
           </Row>
-          </Container>
-        </div>
-      );
+      <Row>
+
+      </Row>
+    </div>
+        </Col>
+        </Row>
+        </Container>
+      </div>
+    );
+   }else {
+    this.fetchProfile();
+    this.fetchContract();
+    return (<center> <h6> Loading..: <Loader
+    type="Puff"
+    color="black"
+    height={100}
+    width={100}
+    timeout={3000} //3 secs
+
+    /></h6></center>);
+   }
+  
+      
     }
   else{
+    
       if((this.state.superWeb3==null||this.state.superContract ==null)&&(this.props.web3!=null)){
         this.state.superWeb3= this.props.web3;
         this.state.superContract=this.props.contract;
@@ -84,100 +171,3 @@ export default class Profile extends React.Component {
   }
 }
 
-class ProfileView extends React.Component {
-  constructor(props) {
-    super(props);
-   
-    this.state = {
-      superWeb3:this.props.web3,
-      superContract:this.props.contract,
-    };
-  }
-  
-  render(){
-  //  if(this.state.superWeb3!= null && this.state.superContract !=null){
-      if(1){
-  //     const cbox = ({ customFields, box, "space", "0x2Ee331840018465bD7Fe74aA4E442b9EA407fBBE", myProfile, redirectFn }) => (
-  //       <EditProfile
-  //            // required
-  //            box={box}
-  //            space={space}
-  //            currentUserAddr={myAddress}
-  //            // optional
-          
-  //       />
-  //  );
-    return(
-      <div>
-        <Row>
-          <Col sm="12" md="3">
-              <CardImg width="200" src="https://c.gitcoin.co/avatars/0357f94b529985a8a898ab338add0edf/djrosenbaum.png" /> <br /> <br/>
-              <h3><b>Daniel</b></h3>
-              <h5><b><a href="/#/edit-profile" style={{color: "white"}}>Edit Profile</a></b></h5>
-          </Col>
-          <Col sm="12" md="9">
-            <div>
-              <Card>
-                <CardHeader>Details</CardHeader>
-                <CardBody>
-                  <p>Total Tickets :- {"asd"}</p>
-                  <p>Total Events Attended/Attending :- {"asd"}</p>
-        
-                  </CardBody>
-                </Card>
-            </div>
-          </Col>
-        </Row>
-        <br />
-        <br />
-      <hr />
-        <h5>Events You have Added</h5>
-        <Row>
-          <ListGroup>
-            
-          </ListGroup>
-        </Row>
-
-        <br />
-          <hr />
-            <h5>Your Tickets</h5>
-            <Row>
-
-             
-            </Row>
-        <Row>
-
-        </Row>
-      </div>
-    );
-    }  else {
-      if((this.state.superWeb3==null||this.state.superContract ==null)&&(this.props.web3!=null)){
-        this.state.superWeb3= this.props.web3;
-        this.state.superContract=this.props.contract;
-        //this.state.accounts=this.props.account;
-      }
-      return(<div>
-        <Container className="main-container">
-          <Row>
-            <Col sm="12" md="12">
-              <div>
-        
-                <Card>
-                 
-                  
-                  <CardBody className="WEB3">
-                 <center>   <CardTitle>Please Connect to Web3</CardTitle>
-                    </center>
-                  </CardBody>
-                </Card>
-              </div>
-            </Col>
-          </Row>
-        </Container>
-      </div>
-     );
-    
-    }
-  }
-
-}
